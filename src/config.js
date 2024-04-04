9f8577f399bdc8a8a96c3804ef20a00274971a4c
KoolMohPC <justmohammad66@gmail.com>
2024-04-04 19:17:47 +0200

    More DB bug fixes

--- src/config.js
+++ src/config.js
@@ -1,8 +1,6 @@
 module.exports = {
   port: process.env["PORT"],
-  mongoose: {
-    URI: `mongodb+srv://koolmoh:${process.env.DB_PASSWORD}@cluster0.ecy96sv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
-  },
+  mongooseURI: `mongodb+srv://koolmoh:${process.env.DB_PASSWORD}@cluster0.ecy96sv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
   jwt: {
     accessSecret: process.env["JWT_ACCESS_SECRET"],
     refreshSecret: process.env["JWT_REFRESH_SECRET"],
